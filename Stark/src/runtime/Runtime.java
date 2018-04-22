fc8b5676568c1ab627c95e92c07988bec1b75945
jananit2411 <jthiaga1@asu.edu>
2018-04-22 01:00:07 -0700

    Runtime logic to handle simple mathematical and relational expression

--- Stark/src/runtime/Runtime.java
+++ Stark/src/runtime/Runtime.java
@@ -11,93 +11,140 @@ import java.util.Stack;
 
 public class Runtime {
 
-    String intermediateFilePath;
-    Stack<Integer> intStack = new Stack<Integer>();
-    Stack<Boolean> boolStack = new Stack<Boolean>();
+	String intermediateFilePath;
+	Stack<Integer> intStack = new Stack<Integer>();
+	Stack<Boolean> boolStack = new Stack<Boolean>();
 	Map<String, Integer> intMap = new HashMap<String, Integer>();
 	Map<String, Boolean> boolMap = new HashMap<String, Boolean>();
-	//List<String> intList =new ArrayList<String>();
-	//List<String> boolList =new ArrayList<String>();
+	// List<String> intList =new ArrayList<String>();
+	// List<String> boolList =new ArrayList<String>();
 
-    public static void main(String args[]) {
-        Runtime runtime = new Runtime("data\\intermediate_code\\assignIC.txt");
-    }
+	public static void main(String args[]) {
+		Runtime runtime = new Runtime("data\\intermediate_code\\assignIC.txt");
+	}
 
-    public Runtime(String path) {
-        intermediateFilePath = path;
-        EvaluateProgram();
-    }
+	public Runtime(String path) {
+		intermediateFilePath = path;
+		EvaluateProgram();
+	}
 
-    public void EvaluateProgram() {
-    	String line = null;
-        try {
-            FileReader reader = new FileReader(intermediateFilePath);
-            BufferedReader bufferReader = new BufferedReader(reader);
-            while((line = bufferReader.readLine()) != null) {
-				if(line.startsWith("DECINT")){
-					String var=line.split(" ")[1];
+	public void EvaluateProgram() {
+		String line = null;
+		try {
+			FileReader reader = new FileReader(intermediateFilePath);
+			BufferedReader bufferReader = new BufferedReader(reader);
+			while ((line = bufferReader.readLine()) != null) {
+				if (line.startsWith("DECINT")) {
+					String var = line.split(" ")[1];
 					intMap.put(var, 0);
-					//intList.add(var);
-				}
-				else if(line.startsWith("DECBOOL")){
-					String var=line.split(" ")[1];
+					// intList.add(var);
+				} else if (line.startsWith("DECBOOL")) {
+					String var = line.split(" ")[1];
 					boolMap.put(line.split(" ")[1], false);
-					//boolList.add(var);
-				}
-				else if(line.startsWith("STORE")){	
-					String var=line.split(" ")[1];
-					if(intMap.containsKey(var)) {
+					// boolList.add(var);
+				} else if (line.startsWith("STORE")) {
+					String var = line.split(" ")[1];
+					if (intMap.containsKey(var)) {
 						intMap.put(line.split(" ")[1], intStack.pop());
-					}
-					else if(boolMap.containsKey(var)) {
+					} else if (boolMap.containsKey(var)) {
 						boolMap.put(line.split(" ")[1], boolStack.pop());
 					}
-				}
-				else if(line.startsWith("PUSH")){	
-					String str=line.split(" ")[1];
-					if(checkInt(str)) {
-						intStack.push(Integer.parseInt(str));	
-					}
-					else {
+				} else if (line.startsWith("PUSH")) {
+					String str = line.split(" ")[1];
+					if (checkInt(str)) {
+						intStack.push(Integer.parseInt(str));
+					} else {
 						boolStack.push(Boolean.parseBoolean(str));
 					}
-				}
-				else if(line.startsWith("DISPLAY")){	
-					String str=line.split(" ")[1];
-					if(intMap.containsKey(str)) {
-						System.out.println(intStack.pop());	
-						
-					}
-					else if(boolMap.containsKey(str)) {
+				} else if (line.startsWith("DISPLAY")) {
+					String str = line.split(" ")[1];
+					if (intMap.containsKey(str)) {
+						System.out.println(intStack.pop());
+
+					} else if (boolMap.containsKey(str)) {
 						System.out.println(boolStack.pop());
 					}
-				}
-				else if(line.startsWith("LOAD")){
-					String str=line.split(" ")[1];
-					if(intMap.containsKey(str)) {
-						intStack.push(intMap.get(str));	
-					}
-					else if(boolMap.containsKey(str)) {
+				} else if (line.startsWith("LOAD")) {
+					String str = line.split(" ")[1];
+					if (intMap.containsKey(str)) {
+						intStack.push(intMap.get(str));
+					} else if (boolMap.containsKey(str)) {
 						boolStack.push(boolMap.get(str));
 					}
+				} else if (line.startsWith("ADD")) {
+					intStack.push(intStack.pop() + intStack.pop());
+				} else if (line.startsWith("SUB")) {
+					intStack.push(intStack.pop() - intStack.pop());
+				} else if (line.startsWith("MUL")) {
+					intStack.push(intStack.pop() * intStack.pop());
+				} else if (line.startsWith("DIV")) {
+					int x = intStack.pop();
+					int y = intStack.pop();
+					if (x > 0)
+						intStack.push(y / x);
+					else
+						System.out.println("ERROR:Cannot Divide by zero");
+				} else if (line.startsWith("EQ")) {
+					int firstValue = intStack.pop();
+					int secondValue = intStack.pop();
+					boolean result = false;
+					if (firstValue == secondValue) {
+						result = true;
+					}
+					boolStack.push(result);
+				} else if (line.startsWith("NEQ")) {
+					int firstValue = intStack.pop();
+					int secondValue = intStack.pop();
+					boolean result = false;
+					if (firstValue != secondValue) {
+						result = true;
+					}
+					boolStack.push(result);
+				} else if (line.startsWith("LT")) {
+					int firstValue = intStack.pop();
+					int secondValue = intStack.pop();
+					boolean result = false;
+					if (firstValue < secondValue) {
+						result = true;
+					}
+					boolStack.push(result);
+				} else if (line.startsWith("LTE")) {
+					int firstValue = intStack.pop();
+					int secondValue = intStack.pop();
+					boolean result = false;
+					if (firstValue <= secondValue) {
+						result = true;
+					}
+					boolStack.push(result);
+				} else if (line.startsWith("GT")) {
+					int firstValue = intStack.pop();
+					int secondValue = intStack.pop();
+					boolean result = false;
+					if (firstValue > secondValue) {
+						result = true;
+					}
+					boolStack.push(result);
+				} else if (line.startsWith("LTE")) {
+					int firstValue = intStack.pop();
+					int secondValue = intStack.pop();
+					boolean result = false;
+					if (firstValue >= secondValue) {
+						result = true;
+					}
+					boolStack.push(result);
 				}
-				else if(line.startsWith("ADD")){
-					//String str=line.split(" ")[1];
-					intStack.push(intStack.pop()+intStack.pop());
-						
-				}
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    boolean checkInt(String value) {  
-        try{  
-            Integer.parseInt(value);  
-            return true;  
-         }catch (NumberFormatException e) {  
-            return false;  
-         }  
-   }
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	boolean checkInt(String value) {
+		try {
+			Integer.parseInt(value);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
 }
